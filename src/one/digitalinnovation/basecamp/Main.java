d082e2b811de18d385e572c8f1adf123fda9fca6
Roberlanio-Developer <fabiano.eng@hotmail.com>
2023-02-01 21:08:16 -0300

    metodo_sobrecarga

--- src/one/digitalinnovation/basecamp/Main.java
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -4,12 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Quadrilátero
-        System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d,5d);
-        Quadrilatero.area(7,8,9);
-        Quadrilatero.area(5f,5f);
+        // Quadrilátero de retorno
 
+        System.out.println("Exercício quadrilátero");
+        double areaLosango = Quadrilatero.area(5f,5f);
+        System.out.println("Área do losango: " + areaLosango);
+        double aereaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do Quadrado: "+ aereaQuadrado);
+        double areaRetangulo = Quadrilatero.area(5d,5d);
+        System.out.println("Área de retângulo: "+ areaRetangulo);
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+       
     }
 }
